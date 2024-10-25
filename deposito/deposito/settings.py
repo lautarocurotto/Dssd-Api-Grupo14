bc879dda1d1f74d3f3be3024c316e8b12d0ea4aa
lautarocurotto <lautarocurotto@yahoo.com.ar>
2024-10-25 18:43:11 -0300

    agrego static root

--- deposito/deposito/settings.py
+++ deposito/deposito/settings.py
@@ -142,6 +142,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'deposito/static'),  # Asegúrate de que esta ruta sea correcta
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Este es el directorio donde se recopilan los archivos estáticos
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
